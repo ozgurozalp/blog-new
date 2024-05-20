b4505e9d5d043f2e83dfe4879b841f4c92806246
Özgür ÖZALP <ozgurozalp1999@gmail.com>
2024-05-20 19:27:34 +0300

    update

--- src/components/shared/AppHeader.tsx
+++ src/components/shared/AppHeader.tsx
@@ -77,12 +77,30 @@ export default function AppHeader() {
               width="80"
             >
               <path
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="5.5"
+                strokeLinecap="round"
+                strokeDasharray="40 160"
                 className="line top"
                 d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
               />
-              <path className="line middle" d="m 30,50 h 40" />
               <path
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="5.5"
+                strokeLinecap="round"
+                strokeDasharray="40 142"
+                className="line middle"
+                d="m 30,50 h 40"
+              />
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="5.5"
+                strokeLinecap="round"
                 className="line bottom"
+                strokeDasharray="40 85"
                 d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
               />
             </svg>
